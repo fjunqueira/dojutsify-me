73a6f4f7cbdf5b0eff5be9c67262e2ec1c2ff21b
Fábio Blumm Junqueira <fabio.blumm18@gmail.com>
2017-07-15 21:55:45 -0300

    Removing debug code

--- src/DojutsifyMe/DojutsifyMe.fs
+++ src/DojutsifyMe/DojutsifyMe.fs
@@ -21,20 +21,11 @@ open System.Reactive.Concurrency
 open System.Reactive
 open System.Threading
 
-let mainBox = new ImageBox(Location=Point(0,20), Size=Size(500,500), Image=null)
-let secondBox = new ImageBox(Location=Point(500,0), Size=Size(300,250), Image=null)
-let thirdBox = new ImageBox(Location=Point(500,250), Size=Size(300,250), Image=null)
-let message = new Label(Location=Point(0,0),Size=Size(500,13))
-
-let display (imageBox:ImageBox) (image:Mat) = 
-    imageBox.Image <- image
+let mainBox = new ImageBox(Location=Point(0,0), Size=Size(500, 500), Image=null)
 
 let retrieveFrame channel (capture:VideoCapture) =
     let frame = new Mat()
     (capture.Retrieve(frame, channel),frame)
-
-let drawRectangle color (frame:Mat) rectangle =
-    CvInvoke.Rectangle(frame, rectangle, Bgr(color).MCvScalar, 2)
 
 let tryDetectFace (frame:Mat) =
     let grayscaled = frame |> grayScale
@@ -44,20 +35,11 @@ let tryDetectFace (frame:Mat) =
         tryExtractFace |> 
         (fun maybeFace -> maybeFace, frame, grayscaled)
 
-let getFeatures (face, frame, grayscaled) = 
+let getFeatures ((face, eyes), frame, grayscaled) = 
         
-    face |> 
-        snd |> 
-        List.toArray |> 
+    eyes |> List.toArray |> 
         Array.collect (goodFeaturesToTrack grayscaled) |> 
-        (fun features -> frame, features) |>
-        (fun ((frame, features) as data) -> 
-            let output = new Mat();
-            let keypoints = new VectorOfKeyPoint(features |> Array.map (fun p -> MKeyPoint(Point=p)))
-            Features2DToolbox.DrawKeypoints(frame, keypoints, output, Bgr(Color.Green),Features2DToolbox.KeypointDrawType.Default)
-            CvInvoke.Resize(output, output, Size(300, 150), 0.0, 0.0, Inter.Linear)
-            secondBox.Image <- output
-            data)
+        (fun features -> frame, features)
 
 let imageGrabbedObservable (capture:VideoCapture) = 
     capture.ImageGrabbed |> 
@@ -70,7 +52,7 @@ let imageGrabbedObservable (capture:VideoCapture) =
 let useFrame status error = 
     let totalMissingFeatures = status |> Array.filter ((=)(Convert.ToByte 0)) |> Array.length
     let totalError = error |> Array.sum
-    not (totalError > 75.0f || totalMissingFeatures > 0)                
+    not (totalError > 75.0f || totalMissingFeatures > 0)        
 
 let trackFeatures (previousFrame, previousFeatures) (currentFrame, currentFeatures) =
     match currentFeatures |> Array.length with
@@ -85,7 +67,7 @@ let trackFeatures (previousFrame, previousFeatures) (currentFrame, currentFeatur
 let main args = 
     let form = new Form(Width=800, Height=500, Name="Dojutsify Me")
     
-    form.Controls.AddRange([|mainBox;secondBox;thirdBox;message|])
+    form.Controls.AddRange([|mainBox|])
 
     let capture = new VideoCapture()
     capture.Start()
@@ -113,6 +95,7 @@ let main args =
             Observable.scan trackFeatures |>
             Observable.observeOn controlScheduler |>    
             Observable.subscribe (fun (frame, points) -> 
+
                 let output = new Mat();
                 let keypoints = new VectorOfKeyPoint(points |> Array.map (fun p -> MKeyPoint(Point=p)))
                 Features2DToolbox.DrawKeypoints(frame, keypoints, output, Bgr(Color.Green),Features2DToolbox.KeypointDrawType.Default)
